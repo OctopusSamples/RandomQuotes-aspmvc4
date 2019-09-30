5158e2162f913a951acf521aef6542dcafcd1aa4
Matthew Casperson <matthewcasperson@gmail.com>
2019-09-30 11:08:53 +1000

    Added a test

---
+++ RandomQuotes.Tests/Tests.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using NUnit.Framework;
+using RandomQuotes.Models;
+
+namespace RandomQuotes.Tests
+{
+    [TestFixture]
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+            using (new FakeHttpContext.FakeHttpContext())
+            {
+                QuoteLoader.Initialize();
+            }
+        }
+
+        [Test]
+        public void GetQuote()
+        {
+            Assert.That(Quote.GetRandomQuote().QuoteText != "Something went wrong");
+        }
+    }
+}
